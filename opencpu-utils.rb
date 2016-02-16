4890a2989d7ead1b1187b7613d7a7a6d4428360b
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2016-02-16 11:45:10 +0200

    Upgraded to opencpu-utils v0.0.3

--- opencpu-utils.rb
+++ opencpu-utils.rb
@@ -1,9 +1,10 @@
 class OpencpuUtils < Formula
   homepage "https://github.com/vst/opencpu-utils"
-  url "https://github.com/vst/opencpu-utils/archive/0.0.2.tar.gz"
-  sha1 "3afda0631581a6a9167cf239f1b195b8bbd3899c"
+  url "https://github.com/vst/opencpu-utils/archive/0.0.3.tar.gz"
+  sha1 "9187d7c237bc5b1f03742a459cff0bba6b9846e6"
 
   def install
+    bin.install "copencpu"
     bin.install "copencpu-version"
     bin.install "copencpu-render"
   end
