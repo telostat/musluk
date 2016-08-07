62c45489252b089330e750c9380a2109e2dbd178
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2016-08-07 12:46:53 +0800

    Using sha256 instead of deprecated sha1

--- opencpu-utils.rb
+++ opencpu-utils.rb
@@ -1,7 +1,7 @@
 class OpencpuUtils < Formula
   homepage "https://github.com/vst/opencpu-utils"
   url "https://github.com/vst/opencpu-utils/archive/0.0.3.tar.gz"
-  sha1 "9187d7c237bc5b1f03742a459cff0bba6b9846e6"
+  sha1 "151b6809a21c1a676775482b23b236ede896ea611cb318b3ceec33edb26baf3b"
 
   def install
     bin.install "copencpu"
