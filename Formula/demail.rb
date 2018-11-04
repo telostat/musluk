775827139c81ab2ec35d9425b54a28a5a94246f3
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2018-11-04 08:27:15 +0800

    [update] demail v0.0.2

--- Formula/demail.rb
+++ Formula/demail.rb
@@ -1,14 +1,14 @@
 class Demail < Formula
   homepage "https://github.com/vst/demail"
-  url "https://github.com/vst/demail/releases/download/0.0.1/demail-assembly-0.0.1.jar", :using => NoUnzipCurlDownloadStrategy
-  sha256 "fd56537f2c93d1da98fda4aca0a41cd02ee053fa4681760aa960e8effc8d462c"
+  url "https://github.com/vst/demail/releases/download/0.0.1/demail-assembly-0.0.2.jar", :using => NoUnzipCurlDownloadStrategy
+  sha256 "98df0165cd52c0c9ebf9e998ab5cd5a49682033844bfdda56dac56c22a8f13e7"
 
   def install
-    libexec.install "demail-assembly-0.0.1.jar"
+    libexec.install "demail-assembly-0.0.2.jar"
 
     (bin/"demail").write <<~EOS
       #!/bin/sh
-      exec java -jar "#{libexec}/demail-assembly-0.0.1.jar" "$@"
+      exec java -jar "#{libexec}/demail-assembly-0.0.2.jar" "$@"
     EOS
   end
 end
